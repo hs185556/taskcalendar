1f87cc0dee6305c5e5485e7d7d52e88ff8491c96
hs185556 <410262593@qq.com>
2023-01-11 13:48:51 +0800

    feat: 日历手势

--- src/pages/MyCalendar/index.tsx
+++ src/pages/MyCalendar/index.tsx
@@ -11,6 +11,9 @@ import { getTaskCountInMonthSync } from "@/services/Task";
 export default forwardRef((props, ref) => {
   const [date, setDate] = useState();
   const [delayDays, setDelayDays] = useState([]);
+  const [startX, setStartX] = useState(-1);
+  const [endX, setEndX] = useState(-1);
+  const [YM, setYM] = useState();
   const calRef = React.createRef();
 
   useImperativeHandle(ref, () => ({
@@ -31,6 +34,44 @@ export default forwardRef((props, ref) => {
     setDelayDays(mCounts);
   };
 
+  /*手接触屏幕*/
+  const handleTouchStart = (e) => {
+    setStartX(e.touches[0].clientX);
+  };
+  /*手在屏幕上移动*/
+  const handleTouchMove = (e) => {
+    setEndX(e.touches[0].clientX);
+  };
+  /*手离开屏幕*/
+  const handleTouchEnd = (e) => {
+    // 获取滑动范围
+    if (startX > -1 && endX > -1) {
+      let distance = Math.abs(startX - endX);
+      if (distance > 50) {
+        if (startX > endX) {
+          // 右滑
+          handleChangePanel(1);
+        } else {
+          // 左滑
+          handleChangePanel(-1);
+        }
+      }
+    }
+
+    setStartX(-1);
+    setEndX(-1);
+  };
+
+  const handleChangePanel = (offset) => {
+    const date1 = YM ? new Date(YM) : new Date();
+    date1.setMonth(date1.getMonth() + offset)
+    const val = `${date1.getFullYear()}-${(date1.getMonth() + 1) < 10 ? '0' + (date1.getMonth() + 1) : (date1.getMonth() + 1)}`
+    // console.log(val);
+    setYM(val);
+    calRef.current?.jumpTo?.({ year: date1.getFullYear(), month: date1.getMonth() + 1 })
+    queryDelayTask(val, [0, 1, 2]);
+  }
+
   useEffect(() => {
     setTimeout(() => {
       // 延迟查询 等待数据库实例化完成
@@ -44,7 +85,12 @@ export default forwardRef((props, ref) => {
   }, [date]);
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <Calendar
         ref={calRef}
         selectionMode="single"
@@ -55,8 +101,9 @@ export default forwardRef((props, ref) => {
           }
         }}
         onPageChange={(year, month) => {
-          const YM = year + "-" + (month < 10 ? "0" + month : month);
-          queryDelayTask(YM, [0, 1, 2]);
+          const val = year + "-" + (month < 10 ? "0" + month : month);
+          setYM(val);
+          queryDelayTask(val, [0, 1, 2]);
         }}
         renderLabel={(date: Date) => {
           const item = delayDays[date.getDate() - 1];
